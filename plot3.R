4bf2d5339d8a598f4ab8ecd29d7e9e8f0a4f0d76
JammiNRao <jammi2801@gmail.com>
2014-05-11 20:46:48 +0100

    further tweaks to scripts to get legends right

--- plot3.R
+++ plot3.R
@@ -1,12 +1,16 @@
 # Plot 2 a line plot of Global Active Power against time for the 2 days
 # 2007 feb 1 and feb 2.
 ## Next line read in the data into object power.data
-power.data <- read.table("household_power_consumption.txt", sep = ";", header = TRUE, colClasses = classes, na.strings = "?")
+power.data <- read.table("C:/Users/Gorway Global/Google Drive/Coursera/Data Science Specialisation/Exploratory data analysis/Project1/household_power_consumption.txt", sep = ";", header = TRUE, na.strings = "?")
 feb2007.data <- subset(power.data, Date == "1/2/2007" | Date == "2/2/2007")
 feb2007.data$datetime <- as.POSIXct(strptime(paste(feb2007.data$Date, feb2007.data$Time), "%d/%m/%Y %H:%M:%S" ))
-x <- feb2007.data$datetime; y <- feb2007.data$Sub_metering_1
-png(filename="C:/Users/Gorway Global/Google Drive/Coursera/Data Science Specialisation/Exploratory data analysis/Project1/plot3.png")
-plot(x,y, type="l", xlab ="", ylab = "Energy sub metering")
-lines(x, feb2007.data$Sub_metering_2, type ="l", col="red")
-lines(x, feb2007.data$Sub_metering_3, type ="l", col="blue")
+x <- feb2007.data$datetime; a <- feb2007.data$Sub_metering_1
+b <- feb2007.data$Sub_metering_2; c <- feb2007.data$Sub_metering_3
+png(filename="plot3.png")
+plot(x,a, type="n", xlab ="", ylab = "Energy sub metering")
+lines(x, a, type ="l", col ="black")
+lines(x, b, type ="l", col="red")
+lines(x, c, type ="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+       lty = c(1,1,1), lwd = c(2,2,2), col = c("black", "red", "blue"))
 dev.off()
--- plot4.R
+++ plot4.R
@@ -5,16 +5,25 @@
 power.data <- read.table("household_power_consumption.txt", sep = ";", header = TRUE, colClasses = classes, na.strings = "?")
 feb2007.data <- subset(power.data, Date == "1/2/2007" | Date == "2/2/2007")
 feb2007.data$datetime <- as.POSIXct(strptime(paste(feb2007.data$Date, feb2007.data$Time), "%d/%m/%Y %H:%M:%S" ))
-x <- feb2007.data$datetime; y <- feb2007.data$Sub_metering_1; z <- feb2007.data$Voltage
-a <- feb2007.data$Sub_metering_1; b <- feb2007.data$Sub_metering_2; C <- feb2007.data$Sub_Metering_3
-png(filename="C:/Users/Gorway Global/Google Drive/Coursera/Data Science Specialisation/Exploratory data analysis/Project1/plot4.png")
+x <- feb2007.data$datetime
+y <- feb2007.data$Global_active_power
+z <- feb2007.data$Voltage
+a <- feb2007.data$Sub_metering_1
+b <- feb2007.data$Sub_metering_2
+C <- feb2007.data$Sub_Metering_3
+png(filename="plot4.png")
 oldpar <- par(mfrow = c(2,2))
-y <- feb2007.data$Global_active_power; x <- feb2007.data$datetime
-plot(x, y, xlab = "", ylab = "Global Active Power", type = "l",)
-plot(x,z,xlab = "datetime", ylab = "Voltage", type = "l")
-plot(x,a, type="l", xlab ="", ylab = "Energy sub metering")
-lines(x, feb2007.data$Sub_metering_2, type ="l", col="red")
-lines(x, feb2007.data$Sub_metering_3, type ="l", col="blue")
-plot(x,y, type ="h", xlab ="datetime", ylab = "Global_active_power")
+plot(x, y, xlab = "", ylab = "Global Active Power", type = "l",) # this is the first plot
+plot(x,z,xlab = "datetime", ylab = "Voltage", type = "l") # this is the second plot
+plot(x,a, type="n", xlab ="", ylab = "Energy sub metering")
+lines(x, a, type ="l", col ="black")
+lines(x, b, type ="l", col="red")
+lines(x, c, type ="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+       lty = c(1,1,1), lwd = c(2,2,2), col = c("black", "red", "blue"))
+
+plot(x,y, type ="h", xlab ="datetime", ylab = "Global_active_power") # the 4th plot
 par(oldpar)
 dev.off()
+# end of code
+
